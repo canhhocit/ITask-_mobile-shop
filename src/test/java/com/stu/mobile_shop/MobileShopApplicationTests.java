12fc36b4bce22ea710eff86eaa0fc16ab5336e49
canhhocit <canhhuu26@gmail.com>
2026-02-18 22:04:53 +0700

    first commit

---
+++ src/test/java/com/stu/mobile_shop/MobileShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stu.mobile_shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MobileShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
